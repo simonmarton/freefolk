595fa1693df8a4cc7097f96da711235a77010fb8
Simon Márton <simon.marton@gmail.com>
2019-04-03 23:38:24 +0200

    simple app
    Signed-off-by: Simon Márton <simon.marton@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Who dies next in GoT?</h2>
+    <div class="character-list">
+      <Character
+        v-for="({ id, name, avatar, isAlive }) in characters"
+        v-bind:key="id"
+        v-bind:name="name"
+        v-bind:avatar="avatar"
+        v-bind:isAlive="isAlive"
+        v-on:click.native="killCharacter(id)"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import characters from "./characters-short.json";
+import Character from "./components/Character.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: () => ({
+    characters
+  }),
+  methods: {
+    killCharacter: function(characterId) {
+      const char = this.$data.characters.find(({ id }) => id === characterId);
+      char.isAlive = false;
+    }
+  },
   components: {
-    HelloWorld
+    Character
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: darkgray;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.character-list {
+  display: flex;
+  flex-flow: wrap;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Character.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="character" v-bind:class="{ dead: !isAlive }">
+    <img :src="avatar" :alt="name" class="avatar">
+    <span class="name">{{name}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Character",
+  props: {
+    avatar: String,
+    name: String,
+    isAlive: Boolean
+  }
+};
+</script>
+
+<style>
+.character {
+  display: flex;
+  align-items: center;
+  width: 400px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 50px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+  background: white;
+  cursor: pointer;
+  transition: opacity 0.5s;
+}
+
+.avatar {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  object-position: top;
+  border-radius: 50%;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+}
+
+.name {
+  font-size: 24px;
+  margin-left: 1rem;
+}
+
+.character.dead {
+  opacity: 0.4;
+  cursor: default;
+}
+
+.character.dead .name {
+  text-decoration: line-through;
+}
+</style>
+
